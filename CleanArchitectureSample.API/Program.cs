694695f36a30950cf51ad70e76a83c7d815c85c3
Georges Blazakis <priscilablazakis@192-168-1-106.tpgi.com.au>
2020-04-21 16:32:47 +1000

    Add Audit	 fields

--- CleanArchitectureSample.API/Program.cs
+++ CleanArchitectureSample.API/Program.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using CleanArchitectureSample.Infrastructure.Database;
+using Microsoft.EntityFrameworkCore;
 
 namespace CleanArchitectureSample.API
 {
@@ -13,7 +16,22 @@ namespace CleanArchitectureSample.API
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();            
+            using (var scope = host.Services.CreateScope()) {
+
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var usersContext = services.GetRequiredService<UsersContext>();
+                    usersContext.Database.Migrate();
+                }
+                catch (System.Exception ex)
+                {
+                    var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "Error occurred while migrating or initializing the database.");   
+                }
+            }
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
---
+++ CleanArchitectureSample.Infrastructure/Migrations/20200421053430_AddAuditFields.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CleanArchitectureSample.Infrastructure.Migrations
+{
+    public partial class AddAuditFields : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Created",
+                table: "Users",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "CreatedBy",
+                table: "Users",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastModified",
+                table: "Users",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastModifiedBy",
+                table: "Users",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Created",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedBy",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "LastModified",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "LastModifiedBy",
+                table: "Users");
+        }
+    }
+}
